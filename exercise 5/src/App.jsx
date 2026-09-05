34e7b37e80fb0d057884b8bb92e453bd63607eff
mohadaahir <mohadaahir2023@gmail.com>
2026-09-05 19:27:21 +0300

    exercise5

---
+++ exercise 5/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import ShoppingCart from './shoping.jsx';
+
+function App() {
+  return (
+    <div>
+      <ShoppingCart />
+    </div>
+  )
+}
+
+export default App
