f38588c48139851321725f5a8c825f32c4e22c88
mohadaahir <mohadaahir2023@gmail.com>
2026-09-19 21:10:57 +0300

    exercise 11

---
+++ exercise 11/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import LoginForm from './loginform'
+
+function App() {
+
+
+  return (
+<LoginForm/>
+  )
+}
+
+export default App
